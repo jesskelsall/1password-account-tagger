4eea1b2d200859c7818f122b54c8f8675dd68c68
Jessica Kelsall <hi@jesskelsall.dev>
2020-11-13 00:34:29 +0000

    Fixed tag modify function type errors

--- src/tags/modify.ts
+++ src/tags/modify.ts
@@ -1,5 +1,5 @@
 import { OnePasswordTag } from '../clipboard'
-import { PreparedTag, Tag } from './schema'
+import { PreparedTag, ResolvedTag } from './schema'
 
 // Create a new tag for a OnePasswordTag with no associated Tag
 export const createDeletedTag = (value: OnePasswordTag): PreparedTag => ({
@@ -11,20 +11,20 @@ export const createDeletedTag = (value: OnePasswordTag): PreparedTag => ({
 })
 
 // Mark a tag as pre-selected
-export const selectTag = (tag: Tag): PreparedTag => ({
+export const selectTag = (tag: ResolvedTag): PreparedTag => ({
   ...tag,
   selected: true,
 })
 
 // Mark a tag as pre-selected because it was not otherwise selected but is mandatory
-export const addTag = (tag: Tag): PreparedTag => ({
+export const addTag = (tag: ResolvedTag): PreparedTag => ({
   ...tag,
   selected: true,
   update: { action: 'add' },
 })
 
 // Mark a tag as pre-selected because a OnePasswordTag matched with one of its ReplacementMatchers
-export const replaceTag = (oldValue: OnePasswordTag, tag: Tag): PreparedTag => ({
+export const replaceTag = (oldValue: OnePasswordTag, tag: ResolvedTag): PreparedTag => ({
   ...tag,
   selected: true,
   update: {
