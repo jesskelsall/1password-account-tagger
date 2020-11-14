edf7c85526e946656f122e92d4a687fc07af907e
Jessica Kelsall <hi@jesskelsall.dev>
2020-11-14 02:44:39 +0000

    Tidied up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import inquirer from 'inquirer'
 import checkboxPlus from 'inquirer-checkbox-plus-prompt'
-import { readFromClipboard } from './clipboard'
+import { readFromClipboard, writeToClipboard } from './clipboard'
 import { askQuestion, generateInquirerQuestion } from './inquirer/question'
 import { prepareTags, resolveTagValues } from './tags/prepare'
 import { tags } from './tags/tags'
 
 inquirer.registerPrompt('checkbox-plus', checkboxPlus)
 
-const test = async (): Promise<void> => {
+const handleTags = async (): Promise<void> => {
   const clipboardTags = await readFromClipboard()
+
   const resolvedTags = resolveTagValues(tags)
   const preparedTags = prepareTags(resolvedTags, clipboardTags)
+
   const question = generateInquirerQuestion(preparedTags)
+  const selectedTags = await askQuestion(question)
 
-  const finalTags = await askQuestion(question)
-
-  console.info(finalTags)
+  await writeToClipboard(selectedTags)
 }
 
-test()
+handleTags()
