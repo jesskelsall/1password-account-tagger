1e7dc27f9bdc8bf047c8ec1a88e761bda2d442c7
Jessica Kelsall <hi@jesskelsall.dev>
2020-11-14 20:53:16 +0000

    Add --update-processed help description

--- src/options/argv.ts
+++ src/options/argv.ts
@@ -1,12 +1,18 @@
 import { drop, pick } from 'lodash/fp'
+import { terminalWidth } from 'yargs'
 import yargs from 'yargs/yargs'
-import { Options, Argv } from './types'
+import { Argv, Options } from './types'
 
 export const getOptions = (argv: Argv): Options => {
   const { argv: args } = yargs(drop(2, argv))
     .options({
-      'update-processed': { type: 'boolean', default: true },
+      'update-processed': {
+        default: true,
+        description: 'Make the current processed quarter tag mandatory, applying it by default.',
+        type: 'boolean',
+      },
     })
+    .wrap(terminalWidth())
 
   // Cast to the Options type
   // This is because yargs returns both camel and kebab cased option names
